42aac48834bcae7df328d86a8ccdcd9a76b2a511
HayReeJ <abdullah.naseer28@gmail.com>
2022-08-14 21:10:13 +0500

    Push command does the job.

--- Test-after-SSH-Key.R
+++ Test-after-SSH-Key.R
@@ -1,3 +1,6 @@
 print("This is a test print line")
 
 print("Checking if commit does the job or will need push. If this is the last line of project, it means commit does the job")
+
+
+print("No, the commit does not do the job. Push is required")
